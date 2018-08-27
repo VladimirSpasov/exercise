a42a2d5df1f94897af3fbc0a3084af85557803e8
Vladimir <vladimir.spaspov-prestataire@laposte.fr>
2018-08-27 19:42:32 +0200

    Created project and podfile

---
+++ exercise/exercise/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  exercise
+//
+//  Created by SPASOV DIMITROV Vladimir on 27/8/18.
+//  Copyright © 2018 SPASOV DIMITROV Vladimir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
